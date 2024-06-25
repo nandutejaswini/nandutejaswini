58937583d2bd09c14e9c56924adf6876a96ab48a
Tejaswini Nandu <51699657+nandutejaswini@users.noreply.github.com>
2024-06-25 01:00:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -44,3 +44,11 @@ ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact for
 ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
+
+const typed = new Typed('.multiple-text',{
+    strings:[' Machine learning Engineer','Data Scientist'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
